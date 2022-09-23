43c77c29f318a9f530b1803492636f27697f81cd
it1925 <pavelsixta2@seznam.cz>
2022-09-23 11:50:25 +0200

    first commit

---
+++ backend/urls.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('microsoft/', include('microsoft_auth.urls', namespace='microsoft')),
+]
+
+if settings.DEBUG:
+    urlpatterns.extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
+
+urlpatterns.append(path('', include('cms.urls')))
+
+# the new django admin sidebar is bad UX in django CMS custom admin views.
+admin.site.enable_nav_sidebar = False
